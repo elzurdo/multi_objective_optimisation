e73dffa4d8958fd99d04a59b10d377e4bf2b2278
Eyal <eyalkazin@gmail.com>
2021-05-26 08:02:43 +0100

    objectives names, distribution feature, intro expander

--- apps/wack_a_pareto.py
+++ apps/wack_a_pareto.py
@@ -7,26 +7,31 @@ import matplotlib.pyplot as plt
 from operator import lt as less_than, gt as greater_than
 from operator import (truediv as div, mul)
 
-feature1, feature2 = "objective_1", "objective_2"
+value_distribution_mode = st.sidebar.selectbox('Select distribution type', ['uncorrelated','squared correlation'])
+
+feature1_default, feature2_default = "Objective 1", "Objective 2"
+
+feature1 = st.sidebar.text_input('Change Objective 1 name', value= feature1_default)
+feature2 = st.sidebar.text_input('Change Objective 2 name', value= feature2_default)
 
 """
 # Wack a Pareto Front
 
-**In interactive demo to learn to identfiy Pareto Optimal solutions**
+**An interactive demo to learn to identfy Pareto Optimal solutions**
 """
 
 def generate_objectives(n_packages = 20,
                       weight_mean = 0.1, weight_sd = 10,
                       value_mean = 0.1, value_sd = 100,
-                      value_distribution_mode = 'random',
+                      value_distribution_mode = 'uncorrelated',
                       noise_factor = 1./2,
                       seed = 3141592653, visualise=True):
     """
     n_packages: int. the total number of packages to choose from
     weight_mean: float. the mean of the weight distribution
     weight_sd: float. the standard deviation of the weight distribution
-    value_mean: float. when value_distribution_mode='random': the mean of the monetary
-    value_distribution_mode: str. (default: 'random', 'squared')
+    value_mean: float. when value_distribution_mode='uncorrelated': the mean of the monetary
+    value_distribution_mode: str. (default: 'uncorrelated', 'squared')
     noise_factor: float. when value_distribution_mode='squared': the standard deviation of the noise introduced
     """
 
@@ -34,11 +39,11 @@ def generate_objectives(n_packages = 20,
 
     weights = np.abs(np.random.normal(weight_mean, weight_sd, n_packages))
 
-    if 'squared' == value_distribution_mode:
+    if 'squared correlation' == value_distribution_mode:
         values =  weights ** 2
         values += values * np.random.normal(0, noise_factor, len(weights))
         values = np.abs(values)
-    elif 'random' == value_distribution_mode:
+    elif 'uncorrelated' == value_distribution_mode:
         values = np.abs(np.random.normal(value_mean, value_sd, n_packages))
 
     #packages = {idx: {'objective1': weights[idx], 'objective2': values[idx]} for idx in range(n_packages)}
@@ -47,30 +52,29 @@ def generate_objectives(n_packages = 20,
         visualise_packages(packages)
 
 
-    return {"objective_1": weights, "objective_2": values}
+    return {feature1: weights, feature2: values}
 
 
 seed = st.number_input('Wack No. (change this to see a different distribution)', min_value=1, value=1, max_value=1000)
 """*More parameters may be changed on the side bar on the left.*"""
 
 n_packages = st.sidebar.number_input('No. of Solutions', min_value=5, value=50, max_value=400)
-mode_ = st.sidebar.selectbox(f'Optimisation {feature1}, {feature2}', ["min min","min max","max min", "max max"])
+mode_ = st.sidebar.selectbox(f'Optimisation direction of {feature1}, {feature2}', ["min, min","min, max","max, min", "max, max"])
 
 
-
-objective_values = generate_objectives(n_packages, seed=seed, value_distribution_mode="random", visualise=False)
+objective_values = generate_objectives(n_packages, seed=seed, value_distribution_mode=value_distribution_mode, visualise=False)
 
 
 # Objective Space declaration
 
 # CHANGE ME!
-if mode_ == "min max":
+if mode_ == "min, max":
     objective_mode, heuristic, soh_unit = {feature1: 'min', feature2: 'max'}, 'value/weight', '£/kg'
-elif mode_ == "min min":
+elif mode_ == "min, min":
     objective_mode, heuristic, soh_unit = {feature1: 'min', feature2: 'min'}, '1/value/weight', '1/£/kg'
-elif mode_ == "max max":
+elif mode_ == "max, max":
     objective_mode, heuristic, soh_unit = {feature1: 'max', feature2: 'max'}, 'value*weight',  '£*kg'
-elif mode_ == "max min":
+elif mode_ == "max, min":
     objective_mode, heuristic, soh_unit = {feature1: 'max', feature2: 'min'}, 'weight/value', 'kg/£'
 
 
@@ -128,7 +132,7 @@ pareto_idxs = objectives_to_pareto_front(objective_values)
 
 plt.scatter(objective_values[feature1], objective_values[feature2], s=10, alpha=0.7, color='purple')
 
-guess_question = f'How many solutions are Pareto Optimal when optimising for {mode_}?'
+guess_question = f'How many of the {n_packages:,} solutions are Pareto Optimal when optimising for {mode_}?'
 guess = st.text_input(guess_question)
 
 
@@ -141,9 +145,9 @@ if show_pareto and guess != '':
     correct = guess_int == len(pareto_idxs)
 
     if correct:
-        guess_result = f"You are correct! The Pareto Front consists of {guess_int} solutions"
+        guess_result = f"You are correct! The Pareto Front consists of {guess_int} solutions."
     else:
-        guess_result = f"Nope ...the Pareto Front consists of {len(pareto_idxs)} solutions (some might be subtle of the human eye)"
+        guess_result = f"Nope ...the Pareto Front consists of {len(pareto_idxs)} solutions (some might be subtle of the human eye)."
 
     f"""{guess_result}"""
 
@@ -165,6 +169,23 @@ st.pyplot(plt.gcf())
 
 wack_file = "https://www.connections.com/hs-fs/hubfs/WhackAMoleTech.jpg"
 st.image(wack_file) # , width=1000)
+
+
+text_intro = """
+Decision making for multiple objectives is a non-trivial task, especially when they are in conflict. For example, how can one best overcome the classic trade-off between quality and cost of production, when the monetary value of quality is not defined? 
+
+
+When applicable, this Pareto Optimisation method provides better results than the common practice of combining multiple parameters into a single parameter heuristic. 
+The reason for this is quite simple. The single heuristic approach is like horse binders limiting the view of the solution space, whereas Pareto Optimisation enables a bird’s eye view.
+
+Real world applications span from supply chain management, manufacturing, aircraft design, chemical engineering to land use planning and therapeutics discovery.
+"""
+
+paretofront_file = "https://upload.wikimedia.org/wikipedia/commons/2/27/Pareto_Efficient_Frontier_1024x1024.png"
+
+expander_intro = st.beta_expander("Improved Decision Making with Pareto Fronts")
+expander_intro.image(paretofront_file, width=200)
+expander_intro.write(text_intro)
 
 
 vilfredo_file = "https://upload.wikimedia.org/wikipedia/commons/f/fd/Vilfredo_Pareto_1870s2.jpg"
